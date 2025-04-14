085a88108cdcccab3a18328a230b34273d58e1cd
sinhaGuild <18661288+sinhaGuild@users.noreply.github.com>
2025-04-14 16:56:19 +0530

    next config update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["i.ibb.co"],
+    domains: ["i.ibb.co", "firebasestorage.googleapis.com"],
   },
 };
 
